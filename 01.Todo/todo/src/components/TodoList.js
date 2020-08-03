3c081d3b230eab3ca22b97d9d9c88204666786d4
baqudo <baqudo@gmail.com>
2020-08-03 20:21:48 +0300

    props

--- 01.Todo/todo/src/components/TodoList.js
+++ 01.Todo/todo/src/components/TodoList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map(item => {
+        const { id, ...rest } = item;
+    
+        return (
+            <li key={id}>
+                <TodoListItem { ...rest } />
+            </li>
+        )
+    });
+
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            { elements }
         </ul>
     )
 }
--- 01.Todo/todo/src/index.js
+++ 01.Todo/todo/src/index.js
@@ -6,11 +6,18 @@ import SearchPanel from './components/SearchPanel';
 import TodoList from './components/TodoList';
 
 const App = () => {
+
+  const TODO_DATA = [
+    { label: "Drink Tea", important: false, id: 1 },
+    { label: "Drink Coffee", important: true, id: 2 },
+    { label: "Have a Lunch", important: false, id: 3 },
+  ]
+
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={TODO_DATA}/>
     </div>
   )
 } 
