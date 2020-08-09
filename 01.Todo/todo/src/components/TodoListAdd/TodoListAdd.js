f0ca1c80ae385fab6d4c70b945bccaec366fc2bb
baqudo <baqudo@gmail.com>
2020-08-09 16:11:33 +0300

    form field events

--- 01.Todo/todo/src/components/TodoListAdd/TodoListAdd.js
+++ 01.Todo/todo/src/components/TodoListAdd/TodoListAdd.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './TodoListAdd.scss';
 
 export default class TodoListAdd extends Component {
+    state = {
+        label: ''
+    }
+
+    onChange = (e) => {
+        this.setState({ label: e.target.value})
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.label);
+        this.setState({ label: '' });
+    }
 
     render() {
         const { onAdd } = this.props;
@@ -9,22 +22,26 @@ export default class TodoListAdd extends Component {
         let classNames = "todo-list-add d-flex align-items-center mt-3";
 
         return (
-            <div
+            <form
                 className={classNames}
+                onSubmit={ this.onSubmit }
             >
                 <div className="todo-list-add__field">
-                    <input className="todo-list-add__input form-control" />
+                    <input
+                        className="todo-list-add__input form-control"
+                        value={this.state.label}
+                        onChange={this.onChange}
+                        placeholder="What needs to be done?"
+                    />
                 </div>
                 
                 <div className="todo-list-add__buttons ml-2">
 
-                    <button type="button" className="btn btn-outline-primary"
-                        onClick={ () => onAdd('new ToDo') }
-                    >
+                    <button type="submit" className="btn btn-outline-primary">
                         Add item
                     </button>
                 </div>
-            </div>
+            </form>
         )
     }
 }
