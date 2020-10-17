100e547ffabc4e3d738537e05179a1e27732d6bf
baqudo <baqudo@gmail.com>
2020-10-17 18:32:31 +0300

    withRouter

--- 02.Star-db/star-db/src/components/pages/planets-page.js
+++ 02.Star-db/star-db/src/components/pages/planets-page.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
-import Row from '../row';
-import {
-  PlanetsList,
-  PlanetsDetails,
-} from '../sw-components';
+import React from 'react';
+import { PlanetsList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
 
-export default class PlanetsPage extends Component {
-  state = {
-    selectedItem: null
-  }
-
-  onItemSelect = (selectedItem) => {
-    this.setState({ selectedItem })
-  }
-
-  render() {
-    const { selectedItem } = this.state;
-    return (
-      <Row
-        left={<PlanetsList onItemClick={id => this.onItemSelect(id)} />}
-        right={<PlanetsDetails id={selectedItem} />}
-      />
-    )
-  }
+const PlanetsPage = ({ history }) => {
+  return (
+    <PlanetsList
+      onItemClick={(id) => {
+        history.push(`/planets/${id}`);
+      }}
+    />
+  )
 }
+
+export default withRouter(PlanetsPage);
--- 02.Star-db/star-db/src/components/pages/starships-page.js
+++ 02.Star-db/star-db/src/components/pages/starships-page.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
-import Row from '../row';
-import {
-  StarshipsList,
-  StarshipsDetails,
-} from '../sw-components';
+import React from 'react';
+import { StarshipsList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
 
-export default class StarshipsPage extends Component {
-  state = {
-    selectedItem: null
-  }
-
-  onItemSelect = (selectedItem) => {
-    this.setState({ selectedItem })
-  }
-
-  render() {
-    const { selectedItem } = this.state;
-    return (
-      <Row
-        left={<StarshipsList onItemClick={id => this.onItemSelect(id)} />}
-        right={<StarshipsDetails id={selectedItem} />}
-      />
-    )
-  }
+const StarshipsPage = ({ history }) => {
+  return (
+    <StarshipsList
+      onItemClick={(id) => {
+        history.push(`/starships/${id}`);
+      }}
+    />
+  )
 }
+
+export default withRouter(StarshipsPage);
